831721604a0e2624815923003c2ee2ee9fe1ff77
Michael Marchand <35391349+MarchandMD@users.noreply.github.com>
2023-10-23 19:25:13 -0600

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+
+20.times do
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city: Faker::Address.city, state: Faker::Address.state)
+end
